bba21faf0def8635726ec9598346d6104884e5a3
Matteo Garza <matteo.garza@outlook.it>
2025-02-21 15:21:15 +0100

    fix(main): ship selector from useState to useReducer

--- src/components/inputs/shipSelector.tsx
+++ src/components/inputs/shipSelector.tsx
@@ -10,9 +10,18 @@ import {
   Text,
 } from '@mantine/core';
 import { IconSearch } from '@tabler/icons-react';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useReducer } from 'react';
 import { SelectableCardStarship } from './selectableCardStarship';
 import { SkeletonShipList } from './skeletonShipList';
+import { SwapiMultipleResponse } from '@/model/swapiMultipleResponse';
+
+const getShipList = (search = '', page?: 1 | 2 | 3 | 4) => {
+  return fetchStarships({
+    type: 'search',
+    search,
+    page,
+  });
+};
 
 type ShipSelectorProps = {
   onElementClick: (ship: Starship) => void;
@@ -21,43 +30,65 @@ type ShipSelectorProps = {
 export const ShipSelector: React.FC<ShipSelectorProps> = ({
   onElementClick,
 }) => {
-  const [isLoading, setLoading] = useState(false);
-  const [search, setSearch] = useState('');
-  const [page, setPage] = useState<1 | 2 | 3 | 4>(1);
-  const [totalPage, setTotalPage] = useState(4);
-  const [totalResults, setTotalResults] = useState(0);
-  const [starshipList, setStarshipList] = useState<Starship[]>([]);
-
-  const getShipList = (search = '', page?: 1 | 2 | 3 | 4) => {
-    return fetchStarships({
-      type: 'search',
-      search,
-      page,
-    });
+  const initialState = {
+    isLoading: false,
+    search: '',
+    page: 1 as 1 | 2 | 3 | 4,
+    totalPage: 4,
+    totalResults: 0,
+    starshipList: [] as Starship[],
   };
+
+  type State = typeof initialState;
+
+  type Action =
+    | { type: 'SET_LOADING'; payload: boolean }
+    | { type: 'SET_SEARCH'; payload: string }
+    | { type: 'SET_PAGE'; payload: 1 | 2 | 3 | 4 }
+    | { type: 'SET_STARSHIP_LIST'; payload: SwapiMultipleResponse<Starship> };
+
+  const reducer = (state: State, action: Action): State => {
+    switch (action.type) {
+      case 'SET_LOADING':
+        return { ...state, isLoading: action.payload };
+      case 'SET_SEARCH':
+        return { ...state, search: action.payload };
+      case 'SET_PAGE':
+        return { ...state, page: action.payload };
+      case 'SET_STARSHIP_LIST':
+        return {
+          ...state,
+          starshipList: action.payload.results,
+          totalPage: Math.ceil(action.payload.count / 10),
+          totalResults: action.payload.count,
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     const fetchShips = async () => {
-      setLoading(true);
-      const newShipList = await getShipList(search, page);
-      setTotalPage(Math.ceil(newShipList.count / 10));
-      setStarshipList(newShipList.results);
-      setTotalResults(newShipList.count);
-      setLoading(false);
+      dispatch({ type: 'SET_LOADING', payload: true });
+      const newShipList = await getShipList(state.search, state.page);
+      dispatch({ type: 'SET_STARSHIP_LIST', payload: newShipList });
+      dispatch({ type: 'SET_LOADING', payload: false });
     };
     fetchShips();
-  }, [page, search]);
+  }, [state.page, state.search]);
 
   const shipList = useMemo(
     () =>
-      starshipList.map((ship, i) => (
+      state.starshipList.map((ship, i) => (
         <SelectableCardStarship
           key={ship.url + i}
           starship={ship}
           onClick={onElementClick}
         />
       )),
-    [starshipList, onElementClick],
+    [state.starshipList, onElementClick],
   );
 
   return (
@@ -66,22 +97,26 @@ export const ShipSelector: React.FC<ShipSelectorProps> = ({
         leftSectionPointerEvents={'none'}
         leftSection={<IconSearch size={16} />}
         rightSectionPointerEvents={'none'}
-        rightSection={isLoading ? <Loader size={16} /> : <></>}
+        rightSection={state.isLoading ? <Loader size={16} /> : <></>}
         placeholder="e.g. 'Death Star', 'wing'"
-        value={search}
-        onChange={(event) => setSearch(event.currentTarget.value)}
+        value={state.search}
+        onChange={(event) =>
+          dispatch({ type: 'SET_SEARCH', payload: event.currentTarget.value })
+        }
       />
       <Text>
-        {totalResults} result{totalResults !== 1 ? 's' : ''} found.
+        {state.totalResults} result{state.totalResults !== 1 ? 's' : ''} found.
       </Text>
       <SimpleGrid cols={{ base: 1, lg: 2 }} spacing='md'>
-        {isLoading ? <SkeletonShipList length={6} /> : shipList}
+        {state.isLoading ? <SkeletonShipList length={6} /> : shipList}
       </SimpleGrid>
       <Center>
         <Pagination
-          total={totalPage}
-          value={page}
-          onChange={(num) => setPage(num as 1 | 2 | 3 | 4)}
+          total={state.totalPage}
+          value={state.page}
+          onChange={(num) =>
+            dispatch({ type: 'SET_PAGE', payload: num as 1 | 2 | 3 | 4 })
+          }
         ></Pagination>
       </Center>
     </Stack>
